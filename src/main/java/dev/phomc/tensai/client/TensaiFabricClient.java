a9d3056f7acc26fb0ddfd24d37b88446e0ebd679
anhcraft <huynhduyanh123123@gmail.com>
2023-01-22 23:11:25 +0700

    abc

--- src/main/java/dev/phomc/tensai/client/TensaiFabricClient.java
+++ src/main/java/dev/phomc/tensai/client/TensaiFabricClient.java
@@ -24,6 +24,13 @@
 
 package dev.phomc.tensai.client;
 
+import dev.phomc.tensai.client.i18n.CustomTranslationStorage;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +40,24 @@ public class TensaiFabricClient implements ClientModInitializer {
 	public static final String MOD_ID = "tensai-client";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public KeyBinding keyBinding;
+
 	@Override
 	public void onInitializeClient() {
+		CustomTranslationStorage.getInstance().put("key.examplemod.spook", "V Key");
+		CustomTranslationStorage.getInstance().put("category.examplemod.test", "Test ctg");
+
+		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+			"key.examplemod.spook", // The translation key of the keybinding's name
+			InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+			GLFW.GLFW_KEY_V, // The keycode of the key
+			"category.examplemod.test" // The translation key of the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (keyBinding.wasPressed()) {
+				client.player.sendMessage(Text.literal("Key V was pressed!"), false);
+			}
+		});
 	}
 }
